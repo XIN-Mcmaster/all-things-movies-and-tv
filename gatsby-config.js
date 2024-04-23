d4e16b7ec3df2c06680ece4cea4fb3f5a5b56c6d
XIN LYU <nicolexin416@gmail.com>
2024-04-23 12:07:10 -0400

    update search and masonry layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
+ * 
  */
+require("dotenv").config({
+  path: `.env.development`,
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
